5bd7199877feaeb8e794aa4e409701ae1660f9ce
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-06-03 23:56:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function getDOMLevel(element) {
+	let level = 0;
+	while(element){
+		level++;
+		element = element.parentElement;
+	}
+	return level;
+}
+const targetElement = document.getElement('level');
+const domlevel = getDOMLevel(targetElement);
+alert('The level of the element is: ${domlevel}');
